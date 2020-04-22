9ac611a92be96ede6fd3d2422be6164700d327d2
Linh <tran-khanh-linh.nguyen@etu.utc.fr>
2020-04-22 18:24:53 +0200

    supprimer fichier inutile

--- model/model.php
+++
@@ -1,6 +0,0 @@
-<?php
-function getMySqliConnection() {
-    $db_connection_array = parse_ini_file("../config/config.ini");
-    return new mysqli($db_connection_array['DB_HOST'], $db_connection_array['DB_USER'], $db_connection_array['DB_PASSWD'], $db_connection_array['DB_NAME']);
-}
-?>
