46883afee6f3aba5bf1d682d2cbc8f3f65cec822
lyujiawe <jiawen.lyu@etu.utc.fr>
2020-04-27 10:52:23 +0200

    add session.use_only_cookies

--- index.php
+++ index.php
@@ -1,5 +1,8 @@
 <?php
+// interdire la lecture du cookie de session avec un script
   ini_set( 'session.cookie_httponly', 1 );
+// interdire le cookie de session via l'URL (uniquement par cookie)
+  ini_set('session.use_only_cookies', 1);
   session_start();
   
   if(!isset($_SESSION["connected_user"]) || $_SESSION["connected_user"] == "") {
